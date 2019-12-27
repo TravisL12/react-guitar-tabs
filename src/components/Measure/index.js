5c25123882f3cffa6a896765d0a27bcdc79b62d1
Travis Lawrence <travis.lawrence@uber.com>
2019-12-27 15:04:18 -0800

    Add some state

--- src/components/Measure/index.js
+++ src/components/Measure/index.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Measure.css";
 import Chord from "./Chord";
 
 const CHORD_COUNT = 80;
 
-function Measure({ chordCount }) {
+function Measure({ chordCount: chordCountProp }) {
+  const [chordCount, setChordCount] = useState(chordCountProp || CHORD_COUNT)
+
   // Quick way to create an array of N items
   // https://stackoverflow.com/a/38213213
-  const chords = Array.from({ length: chordCount || CHORD_COUNT }, (v, k) => <Chord key={k + 1} />)
+  const chords = Array.from({ length: chordCount }, (v, k) => <Chord key={k + 1} />)
 
   return (
     <div className="Measure">
