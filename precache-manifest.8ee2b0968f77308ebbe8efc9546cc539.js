811dfee2aabc68445c6a8620f030016dbe1c5b8d
Travis Lawrence <travis.lawrence@uber.com>
2019-12-27 15:07:49 -0800

    Updates

---
+++ precache-manifest.8ee2b0968f77308ebbe8efc9546cc539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132e076b78ab8cd1bd3026b198b7f34a",
+    "url": "/react-guitar-tabs/index.html"
+  },
+  {
+    "revision": "8288ba1b74b74843428b",
+    "url": "/react-guitar-tabs/static/css/main.b8fb1c6b.chunk.css"
+  },
+  {
+    "revision": "37023c97379accd2af98",
+    "url": "/react-guitar-tabs/static/js/2.c2b8a30c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-guitar-tabs/static/js/2.c2b8a30c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8288ba1b74b74843428b",
+    "url": "/react-guitar-tabs/static/js/main.ad8f1c2d.chunk.js"
+  },
+  {
+    "revision": "ed28d64c3d3103dd6d74",
+    "url": "/react-guitar-tabs/static/js/runtime-main.f2da8abb.js"
+  }
+]);
