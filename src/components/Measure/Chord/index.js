5bf4c63518df381d6961bc22cf9bbf3df24e7ae8
Travis Lawrence <travis.lawrence@uber.com>
2019-12-29 18:44:41 -0800

    Toggle input works

--- src/components/Measure/Chord/index.js
+++ src/components/Measure/Chord/index.js
@@ -2,35 +2,60 @@ import React, { useState } from "react";
 import "./Chord.css";
 import ChordModel from "../../../Models/chord";
 
+function NoteInput({ value }) {
+  const [showInput, setShowInput] = useState(false);
+  const [noteValue, setNoteValue] = useState(value);
+
+  return (
+    <li className="noteInput">
+      {showInput ? (
+        <input
+          onChange={event => setNoteValue(event.target.value)}
+          onBlur={() => setShowInput(false)}
+          type="text"
+          value={noteValue === "-" ? "" : noteValue}
+          maxLength="2"
+          autoFocus
+        />
+      ) : (
+          <span onClick={() => setShowInput(true)}>{noteValue}</span>
+        )}
+    </li>
+  );
+}
+
 function Chord({ isStaff, isEnd, notes: notesProp }) {
   const [notes, setNotes] = useState(notesProp);
 
   return (
     <div className="Chord">
-      {isStaff || isEnd ? <ul className='staff'>
-        <li>{isStaff && 'e'}|</li>
-        <li>{isStaff && 'B'}|</li>
-        <li>{isStaff && 'G'}|</li>
-        <li>{isStaff && 'D'}|</li>
-        <li>{isStaff && 'A'}|</li>
-        <li>{isStaff && 'E'}|</li>
-      </ul> :
+      {isStaff || isEnd ? (
         <ul>
-          <li>{notes.display.highE}</li>
-          <li>{notes.display.b}</li>
-          <li>{notes.display.g}</li>
-          <li>{notes.display.d}</li>
-          <li>{notes.display.a}</li>
-          <li>{notes.display.lowE}</li>
+          <li>{isStaff && "e"}|</li>
+          <li>{isStaff && "B"}|</li>
+          <li>{isStaff && "G"}|</li>
+          <li>{isStaff && "D"}|</li>
+          <li>{isStaff && "A"}|</li>
+          <li>{isStaff && "E"}|</li>
         </ul>
-      }
+      ) : (
+          <ul>
+            <NoteInput value={notes.display.highE} />
+            <NoteInput value={notes.display.b} />
+            <NoteInput value={notes.display.g} />
+            <NoteInput value={notes.display.d} />
+            <NoteInput value={notes.display.a} />
+            <NoteInput value={notes.display.lowE} />
+          </ul>
+        )}
     </div>
   );
 }
 
 Chord.defaultProps = {
-  notes: new ChordModel(),
-}
+  isStaff: false,
+  isEnd: false,
+  notes: new ChordModel()
+};
 
 export default Chord;
-
